a416f0fc2169fe8710b39801374cda14234ca57a
Nick M. <nickj609@gmail.com>
2024-06-20 15:53:40 -0400

    ADDED: Game Rules

---
+++ CrossCutting/GameRules.cs
@@ -0,0 +1,53 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
+
+namespace GameModeManager
+{
+    public class GameRules : IPluginDependency<Plugin, Config>
+    {
+        CCSGameRules? _gameRules = null;
+
+        public void SetGameRules() => _gameRules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").FirstOrDefault()?.GameRules;
+
+        public void SetGameRulesAsync()
+        {
+            _gameRules = null;
+            new Timer(1.0F, () =>
+            {
+                SetGameRules();
+            });
+        }
+
+        public void OnLoad(Plugin plugin)
+        {
+            SetGameRulesAsync();
+            plugin.RegisterEventHandler<EventRoundStart>(OnRoundStart);
+            plugin.RegisterEventHandler<EventRoundAnnounceWarmup>(OnAnnounceWarmup);
+        }
+
+        public float GameStartTime => _gameRules?.GameStartTime ?? 0;
+
+        public void OnMapStart(string map)
+        {
+            SetGameRulesAsync();
+        }
+
+
+        public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+        {
+            SetGameRules();
+            return HookResult.Continue;
+        }
+
+        public HookResult OnAnnounceWarmup(EventRoundAnnounceWarmup @event, GameEventInfo info)
+        {
+            SetGameRules();
+            return HookResult.Continue;
+        }
+
+        public bool WarmupRunning => _gameRules?.WarmupPeriod ?? false;
+
+        public int TotalRoundsPlayed => _gameRules?.TotalRoundsPlayed ?? 0;
+    }
+}
