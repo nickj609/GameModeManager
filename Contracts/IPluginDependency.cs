e161c474e82a178af4a28b146014b4b5f0061c0a
Nick M. <nickj609@gmail.com>
2024-06-20 15:51:52 -0400

    ADDED: OnMapStart method

--- Contracts/IPluginDependency.cs
+++ Contracts/IPluginDependency.cs
@@ -5,6 +5,8 @@ namespace GameModeManager
     public interface IPluginDependency<TPlugin, TConfig>
     {
         public virtual void OnConfigParsed(TConfig config) { }
+        public virtual void OnMapStart(string mapName) { }
         public virtual void OnLoad(TPlugin plugin) { }
+        public virtual void Clear() { }
     }
 }
